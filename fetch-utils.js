82874cd2c798113774552bd9d68787d764dab854
Danny Cairns <dpcairns@gmail.com>
2021-12-16 09:50:39 -0800

    Update fetch-utils.js

--- fetch-utils.js
+++ fetch-utils.js
@@ -3,14 +3,25 @@ const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsI
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
-export async function createGame(game){
-    const newGame = { ...game };
+/*
+assumptions you can make:
 
+The table name in supabase is `games`
+
+The games are stored in the database using this data model:
+{
+   name1: ,
+   name2: ,
+   score1: ,
+   score2: ,
+}
+*/
+
+export async function createGame(game){
     // create a single new game in the games table using the above object
     
     return checkError(response);
 }
-
 
 export async function getGames() {
     // select all games from the games table
